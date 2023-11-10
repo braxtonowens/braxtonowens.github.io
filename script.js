6e5849a8723a03f0b0887616b70440ba62ad4e14
Braxton Owens <cbraxtonowens@gmail.com>
2023-11-10 16:07:54 -0700

    final commit

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 // Function to toggle fullscreen mode
 function toggleFullScreen() {
+    let timerControl = document.getElementById("timerControl");
+
     if (!document.fullscreenElement) {
         document.documentElement.requestFullscreen();
-        document.getElementById("timerControl").style.display = "none";
+        timerControl.style.display = "none";
     } else {
         if (document.exitFullscreen) {
             document.exitFullscreen();
-            document.getElementById("timerControl").style.display = "block";
+            timerControl.style.display = "block";
         }
     }
 }
@@ -18,13 +20,46 @@ document.addEventListener("keydown", (e) => {
     }
 });
 
-// Function to start timer
+// Function to start and show the countdown timer
 function startTimer(duration) {
+    let timer = duration, minutes, seconds;
+    let timerDisplay = document.createElement("div");
+    document.body.appendChild(timerDisplay);
+    let interval = setInterval(function () {
+        minutes = parseInt(timer / 60, 10);
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        timerDisplay.textContent = minutes + ":" + seconds;
+
+        if (--timer < 0) {
+            clearInterval(interval);
+            timerDisplay.parentNode.removeChild(timerDisplay);
+            flashScreen();
+        }
+    }, 1000);
+}
+
+// Function to flash the screen
+function flashScreen() {
+    document.body.style.backgroundColor = "black";
     setTimeout(function() {
-        document.body.style.backgroundColor = "black";
-        setTimeout(function() {
-            document.body.style.backgroundColor = "white";
-            if (document.fullscreenElement) {
-                toggleFullScreen();
-            }
+        document.body.style.backgroundColor = "white";
+        if (document.fullscreenElement) {
+            toggleFullScreen();
+        }
+    }, 500); // Flash duration
+}
+
+// Event listener for the timer button
+document.getElementById("startTimer").addEventListener("click", function() {
+    var duration = parseInt(document.getElementById("timerInput").value);
+    if (duration > 0) {
+        startTimer(duration);
+    } else {
+        alert("Please enter a valid time in seconds.");
+    }
+});
 
