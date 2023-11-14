83caff872ef8ba12eb66857fa53a17de7bd882e3
Braxton Owens <cbraxtonowens@gmail.com>
2023-11-14 09:15:38 -0700

    final commit

--- script.js
+++ script.js
@@ -20,12 +20,44 @@ document.addEventListener("keydown", (e) => {
     }
 });
 
+document.addEventListener("fullscreenchange", function() {
+    let timerControl = document.getElementById("timerControl");
+    if (!document.fullscreenElement) {
+        timerControl.style.display = "block";
+    }
+});
+
+document.getElementById('timerInput').addEventListener('keydown', function(event) {
+    if (event.key === 'Enter') {
+        var duration = parseInt(document.getElementById("timerInput").value);
+        if (duration > 0) {
+            startTimer(duration);
+        } else {
+            alert("Please enter a valid time in seconds.");
+        }
+    }
+});
+
+let isTimerRunning = false;
+let timerInterval = null; // This will store the interval ID
+let timerDisplay = null;
+
 // Function to start and show the countdown timer
 function startTimer(duration) {
+    if (isTimerRunning) {
+        return; // If the timer is already running, do nothing
+    }
+    isTimerRunning = true;
     let timer = duration, minutes, seconds;
-    let timerDisplay = document.createElement("div");
-    document.body.appendChild(timerDisplay);
-    let interval = setInterval(function () {
+
+    // Create and display the timer element if it doesn't exist
+    if (!timerDisplay) {
+        timerDisplay = document.createElement("div");
+        timerDisplay.id = "timerDisplay";
+        document.body.appendChild(timerDisplay);
+    }
+
+    timerInterval = setInterval(function () {
         minutes = parseInt(timer / 60, 10);
         seconds = parseInt(timer % 60, 10);
 
@@ -35,12 +67,41 @@ function startTimer(duration) {
         timerDisplay.textContent = minutes + ":" + seconds;
 
         if (--timer < 0) {
-            clearInterval(interval);
-            timerDisplay.parentNode.removeChild(timerDisplay);
-            flashScreen();
+            clearInterval(timerInterval);
+            timerInterval = null;
+            isTimerRunning = false;
+            timerDisplay.remove(); // Remove the timer display when done
+            timerDisplay = null;
+            flashScreen(); // Call the function to flash the screen
         }
     }, 1000);
 }
+
+function deleteTimer() {
+    if (timerInterval) {
+        clearInterval(timerInterval); // Stop the timer
+        timerInterval = null;
+    }
+
+    isTimerRunning = false;
+
+    // Remove the timer display element
+    if (timerDisplay) {
+        timerDisplay.remove();
+        timerDisplay = null; // Reset the reference
+    }
+
+    // Optionally, reset or clear the input field
+    const timerInput = document.getElementById("timerInput");
+    if (timerInput) {
+        timerInput.value = "";
+    }
+}
+
+// Event listener for the delete timer button
+document.getElementById('deleteTimer').addEventListener('click', function() {
+    deleteTimer();
+});
 
 // Function to flash the screen
 function flashScreen() {
